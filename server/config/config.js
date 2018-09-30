3e4ead02d6918cde20fafa42ba3bbfeb10850082
Juan Cruz Méndez <jcmendez0907@gmail.com>
2018-09-30 15:47:05 -0600

    Variable de entrorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:As123456@ds133597.mlab.com:33597/jcmendez0907-cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 //urlDB = 'mongodb://cafe-user:As123456@ds133597.mlab.com:33597/jcmendez0907-cafe';
